b83d5c79e1479b6929e8a0980146a7a8bda6abc1
junlongpark <junlongpark@naver.com>
2025-05-13 18:33:29 +0900

    앱

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 # app.py
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 #Flask 객체 인스턴스 생성
 app = Flask(__name__)
@@ -8,7 +8,18 @@ app = Flask(__name__)
 def index():
   return render_template('index.html',user="반원",data={'level':60,'point':360,'exp':45000})
 
+@app.route("/recommend", methods=["POST"])
+def recommend():
+    data = request.get_json()
+    # TODO: 여기에 추천 알고리즘 연결
+    response = {
+        "route": ["경복궁", "광장시장", "DDP"],
+        "total_distance_km": 3.8
+    }
+    return jsonify(response)
+
 if __name__=="__main__":
   app.run(debug=True)
   # host 등을 직접 지정하고 싶다면
   # app.run(host="127.0.0.1", port="5000", debug=True)
+
