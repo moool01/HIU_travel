208b692fca16ced7fec8dfa0e9bd68c71d2b29bf
moool01 <tk90941@gmail.com>
2025-05-13 18:01:34 +0900

    flask update

---
+++ app.py
@@ -0,0 +1,14 @@
+# app.py
+from flask import Flask, render_template
+
+#Flask 객체 인스턴스 생성
+app = Flask(__name__)
+
+@app.route('/') # 접속하는 url
+def index():
+  return render_template('index.html',user="반원",data={'level':60,'point':360,'exp':45000})
+
+if __name__=="__main__":
+  app.run(debug=True)
+  # host 등을 직접 지정하고 싶다면
+  # app.run(host="127.0.0.1", port="5000", debug=True)
